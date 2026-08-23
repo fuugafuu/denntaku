44dd95ae0a9f3c622ca563fcb21dfa377389a040
fuugafuu <fuga2013703@gmail.com>
2026-08-23 15:24:04 +0900

    Bump PWA cache for trailing-equals fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION='math-lens-v1.2.0';
+const VERSION='math-lens-v1.2.1';
 const CORE=['./','./index.html','./styles.css','./app.js','./ocr-worker.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(VERSION).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==VERSION).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
